890c3d1867a8837fd369321cc2929946f6134b6f
serifeturksever <62253012+serifeturksever@users.noreply.github.com>
2023-07-24 15:38:48 +0300

    Initial Commit

---
+++ SwiftNetworkCall/SwiftNetworkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkCallApp.swift
+//  SwiftNetworkCall
+//
+//  Created by Şerife Türksever on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftNetworkCallUITests/SwiftNetworkCallUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftNetworkCallUITestsLaunchTests.swift
+//  SwiftNetworkCallUITests
+//
+//  Created by Şerife Türksever on 24.07.2023.
+//
+
+import XCTest
+
+final class SwiftNetworkCallUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
